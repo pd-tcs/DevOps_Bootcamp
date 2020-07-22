09c4cbdaa7e43c56b80f24355bb2229dd3b72247
pd-tcs <prashantpdave@gmail.com>
2020-07-22 18:05:16 +0530

    Added imporimport org.springframework.web.bind.annotation.GetMapping;

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -1,7 +1,9 @@
 package com;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CalculatorService {
